7193370fb1e2d55687b4a841f174ba791f55b96e
David Cuartas <davidcuba91@gmail.com>
2022-11-30 13:44:05 +0100

    Add mocks for test App

---
+++ src/mocks/experienceMockApp.ts
@@ -0,0 +1,7 @@
+import { ExperienceState } from "../redux/features/experienceSlice/types";
+
+const experienceMockApp: ExperienceState = {
+  experienceList: [],
+};
+
+export default experienceMockApp;
