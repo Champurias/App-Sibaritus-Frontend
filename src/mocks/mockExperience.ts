f8ee958ff892b38bc90575bf5cb8c6edc7cb4117
David Cuartas <davidcuba91@gmail.com>
2022-11-30 16:50:20 +0100

    Add mock experience

---
+++ src/mocks/mockExperience.ts
@@ -0,0 +1,22 @@
+const mockExperience = [
+  {
+    _id: "6385f92759fac43576020b2b",
+    owner: "chebu",
+    picture: "https://www.lagourmeta.com/modules/ph_simpleblog/featured/47.jpg",
+    location: "N@omi",
+    price: 12,
+    detail: "Characters",
+  },
+
+  {
+    _id: "6385fda659fac43576020b2c",
+    owner: "chebu",
+    picture:
+      "https://elmundoenrecetas.s3.amazonaws.com/uploads/recipe/picture/401/I…",
+    location: "N@omi",
+    price: 34,
+    detail: "Characters",
+  },
+];
+
+export default mockExperience;
