0386c32b475b31da3450be663517d5a678d530c8
David Cuartas <davidcuba91@gmail.com>
2022-12-02 19:02:33 +0100

    Add useExperence test

--- src/hooks/useExperience/useExperience.test.ts
+++ src/hooks/useExperience/useExperience.test.ts
@@ -1,7 +1,10 @@
 import { act, renderHook, waitFor } from "@testing-library/react";
 import mockExperience from "../../mocks/mockExperience";
 import ProviderWrapper from "../../mocks/providerWrapper";
-import { getExperienceActionCreator } from "../../redux/features/experienceSlice/experienceSlice";
+import {
+  deleteExperienceActionCreator,
+  getExperienceActionCreator,
+} from "../../redux/features/experienceSlice/experienceSlice";
 import { openModalActionCreator } from "../../redux/features/Uislice/Uislice";
 import { store } from "../../redux/store";
 import { OpenModalActionPayload } from "../../redux/types/types";
@@ -52,6 +55,47 @@ describe("Given the Experience custom hook", () => {
         expect(dispatchSpy).toHaveBeenCalledWith(
           getExperienceActionCreator(mockExperience)
         )
+      );
+    });
+  });
+  describe("When it's method deleteExperience is invoked with experienceId", () => {
+    test("Then it should invoke dispatch with deleteExperienceActionCreator", async () => {
+      const {
+        result: {
+          current: { deleteExperience },
+        },
+      } = renderHook(() => useExperience(), {
+        wrapper: ProviderWrapper,
+      });
+
+      const { _id: experienceIdExpected } = mockExperience[0];
+
+      await deleteExperience(experienceIdExpected as string);
+
+      expect(dispatchSpy).toHaveBeenCalledWith(
+        deleteExperienceActionCreator(experienceIdExpected)
+      );
+    });
+  });
+
+  describe("When it's method deleteExperience is invoked and request not delete expereince", () => {
+    test("Then it should invoke dispatch with openModalActionCreator with error", async () => {
+      const {
+        result: {
+          current: { deleteExperience },
+        },
+      } = renderHook(() => useExperience(), {
+        wrapper: ProviderWrapper,
+      });
+
+      const { _id: experienceIdExpecte } = mockExperience[0];
+      await deleteExperience(experienceIdExpecte as string);
+
+      expect(dispatchSpy).toHaveBeenCalledWith(
+        openModalActionCreator({
+          isError: true,
+          messageFeedback: "hemos tenido un error",
+        })
       );
     });
   });
