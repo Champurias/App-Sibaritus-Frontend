26936421467ffd4c3c7ae064041237c111390cdf
David Cuartas <davidcuba91@gmail.com>
2022-12-03 13:32:25 +0100

    bugfix netlifly deploy

--- src/hooks/useExperience/useExperience.ts
+++ src/hooks/useExperience/useExperience.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useCallback } from "react";
-import { isConstructorDeclaration } from "typescript";
+
 import {
   deleteExperienceActionCreator,
   getExperienceActionCreator,
