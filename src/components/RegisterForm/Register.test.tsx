6fd96be7580ab30e1784249adc82b9c4042b477b
David Cuartas <davidcuba91@gmail.com>
2022-12-05 16:25:40 +0100

    Add styled experience

--- src/components/RegisterForm/Register.test.tsx
+++ src/components/RegisterForm/Register.test.tsx
@@ -1,7 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import providerWrapper from "../../mocks/providerWrapper";
-import ProviderWrapper from "../../mocks/providerWrapper";
 import renderWithProviders from "../../utils/testUtils/renderwithProvider";
 import RegisterForm from "./RegisterForm";
 
