7fa13b41274f389df094d8b8cdb78af38a0b18fe
David Cuartas <davidcuba91@gmail.com>
2022-12-07 10:53:32 +0100

    Add user login in custom hook and test

--- src/hooks/useUser.test.ts
+++ src/hooks/useUser.test.ts
@@ -4,14 +4,24 @@ import { store } from "../redux/store";
 import useUser from "./useUser";
 import { openModalActionCreator } from "../redux/features/Uislice/Uislice";
 import { UserDateRegister } from "../data/types";
-import { OpenModalActionPayload } from "../redux/types/types";
+import {
+  JwtCustomPayload,
+  OpenModalActionPayload,
+  UserLogin,
+  UserLoginData,
+} from "../redux/types/types";
+import { userLoginActionCreator } from "../redux/features/userSlice/userSlice";
+
+jest.mock("jwt-decode", () => {
+  return () => ({ id: "testId", email: "james@gmail.com" } as JwtCustomPayload);
+});
 
 const dispachSpy = jest.spyOn(store, "dispatch");
 
 describe("Given a useUser custom hook", () => {
   const {
     result: {
-      current: { registerUser },
+      current: { registerUser, userLogin },
     },
   } = renderHook(() => useUser(), {
     wrapper: providerWrapper,
@@ -34,24 +44,64 @@ describe("Given a useUser custom hook", () => {
         openModalActionCreator(modalUser)
       );
     });
+  });
 
-    describe("When its method registerUser is invoked with username 'registerUser'", () => {
-      test("Then it should invoke dispatch with openModalAction creator with text 'invalid name, choose another name'", async () => {
-        const registerNewUser: UserDateRegister = {
-          username: "registerUser",
-          password: "1234567",
-          email: "hulio@gmail.com",
-        };
-        const modalUser: OpenModalActionPayload = {
-          isError: true,
-          messageFeedback: "No te has podido registrar",
-        };
-        await registerUser(registerNewUser);
+  describe("When its method registerUser is invoked with username 'registerUser'", () => {
+    test("Then it should invoke dispatch with openModalAction creator with text 'invalid name, choose another name'", async () => {
+      const registerNewUser: UserDateRegister = {
+        username: "registerUser",
+        password: "1234567",
+        email: "hulio@gmail.com",
+      };
+      const modalUser: OpenModalActionPayload = {
+        isError: true,
+        messageFeedback: "No te has podido registrar",
+      };
+      await registerUser(registerNewUser);
 
-        expect(dispachSpy).toHaveBeenCalledWith(
-          openModalActionCreator(modalUser)
-        );
-      });
+      expect(dispachSpy).toHaveBeenCalledWith(
+        openModalActionCreator(modalUser)
+      );
+    });
+  });
+  describe("When the method userLogin is called with email 'james@gmail.com' and password: '1234'", () => {
+    test("Then it should call the dispatch with userLoginActionCreator", async () => {
+      const loginData: UserLogin = {
+        username: "paca@gmail.com",
+        password: "1234",
+      };
+
+      const loginActionPayload: UserLoginData = {
+        id: "testId",
+        token: "tokenTest",
+        email: "james@gmail.com",
+      };
+
+      await userLogin(loginData);
+
+      expect(dispachSpy).toHaveBeenCalledWith(
+        userLoginActionCreator(loginActionPayload)
+      );
+    });
+  });
+
+  describe("When the method userLogin is invoked with email: 'paca@gmail.com' and the incorrect password: '12345'", () => {
+    test("Then it should dispatch the OpenModalActionPayload and text 'No te pudiste logear'", async () => {
+      const newUser: UserLogin = {
+        username: "paca@gmail.com",
+        password: "wrongPassword",
+      };
+
+      const modalPayload: OpenModalActionPayload = {
+        isError: true,
+        messageFeedback: "No te pudiste logear",
+      };
+
+      await userLogin(newUser);
+
+      expect(dispachSpy).toHaveBeenCalledWith(
+        openModalActionCreator(modalPayload)
+      );
     });
   });
 });
--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,24 +1,29 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import { UserDateRegister } from "../data/types";
 import { openModalActionCreator } from "../redux/features/Uislice/Uislice";
+import { userLoginActionCreator } from "../redux/features/userSlice/userSlice";
 import { useAppDispatch } from "../redux/hooks";
+import { JwtCustomPayload, UserLogin } from "../redux/types/types";
+import decodeToken from "../utils/testUtils/decodeToken";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
 const useUser = () => {
-  const dispach = useAppDispatch();
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   const registerUser = async (userRegisterData: UserDateRegister) => {
     try {
       await axios.post(`${apiUrl}/users/register`, userRegisterData);
-      dispach(
+      dispatch(
         openModalActionCreator({
           isError: false,
           messageFeedback: "A la web de sibaritus",
         })
       );
     } catch (error: unknown) {
-      dispach(
+      dispatch(
         openModalActionCreator({
           isError: true,
           messageFeedback: "No te has podido registrar",
@@ -26,7 +31,30 @@ const useUser = () => {
       );
     }
   };
-  return { registerUser };
+  const userLogin = async (loginData: UserLogin) => {
+    try {
+      const loginResponse = await axios.post(
+        `${apiUrl}/users/login`,
+        loginData
+      );
+
+      const { token } = await loginResponse.data;
+      const loggedUser: JwtCustomPayload = decodeToken(token);
+
+      dispatch(userLoginActionCreator({ ...loggedUser, token }));
+      localStorage.setItem("token", token);
+      navigate("/home");
+    } catch (error: unknown) {
+      dispatch(
+        openModalActionCreator({
+          isError: true,
+          messageFeedback: "No te pudiste logear",
+        })
+      );
+    }
+  };
+
+  return { registerUser, userLogin };
 };
 
 export default useUser;
