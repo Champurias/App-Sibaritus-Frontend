0f2a3ba36dc1b47586f51631df09378359003925
David Cuartas <davidcuba91@gmail.com>
2022-12-07 10:54:24 +0100

    Add handlers for testing custom hook

---
+++ src/mocks/userInitialstateMock.ts
@@ -0,0 +1,10 @@
+import { UserState } from "../redux/types/types";
+
+const userInitialStateMock: UserState = {
+  id: "",
+  email: "",
+  isLogged: false,
+  token: "",
+};
+
+export default userInitialStateMock;
