83e0d8e6fec7d87fa5529c52942e5575e952d13f
David Cuartas <davidcuba91@gmail.com>
2022-12-07 10:49:41 +0100

    Add new interface for login

--- src/redux/types/types.ts
+++ src/redux/types/types.ts
@@ -12,3 +12,25 @@ export interface OpenModalActionPayload {
   messageFeedback: string;
   isError: boolean;
 }
+export interface UserLoginData {
+  id: string;
+  token: string;
+  email: string;
+}
+
+export interface UserState extends UserLoginData {
+  isLogged: boolean;
+}
+export interface UserLogin {
+  username: string;
+  password: string;
+}
+export interface JwtCustomPayload {
+  id: string;
+  email: string;
+}
+export interface UserCredentials {
+  name: string;
+  password: string;
+  email: string;
+}
